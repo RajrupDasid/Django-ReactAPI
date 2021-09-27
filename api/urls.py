3915d9b52cb50ec1975c75a510a4497c94e1a93b
RajrupDasid <rajrupdasid@outlook.com>
2021-09-27 14:34:13 +0530

    full backend ready

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import include
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',include('posts.api.urls'))
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
---
+++ posts/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from ..models import Post
+
+class PostModelSerializer(serializers.ModelSerializer):
+    author=serializers.SerializerMethodField()
+    created=serializers.SerializerMethodField()
+    class Meta:
+        model=Post
+        fields=['id','title','description','created','author']
+    def get_author(self,obj):
+        print(self)
+        print(obj)
+        return obj.author.username
+    def get_created(self,obj):
+        return obj.created.strftime("%m-%d-%Y, %H:%M:%S")
